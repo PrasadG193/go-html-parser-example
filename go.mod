ac093f197811f04854f4c6277460c851aeb0258b
Prasad Ghangal <prasad.ghangal@gmail.com>
2022-07-07 17:59:53 +0530

    HTML parser to parse const list from pkg docs page
    Signed-off-by: Prasad Ghangal <prasad.ghangal@gmail.com>

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/PrasadG193/go-html-parser-example
+
+go 1.18
+
+require golang.org/x/net v0.0.0-20220706163947-c90051bbdb60
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+const (
+	pkgDocLink     = "https://pkg.go.dev/k8s.io/component-helpers/storage/volume"
+	sectionToParse = `<section class="Documentation-constants">`
+)
+
+func getPackageDocContent() ([]byte, error) {
+	resp, err := http.Get(pkgDocLink)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	// TODO: Handle non OK response codes
+	return ioutil.ReadAll(resp.Body)
+}
+
+func parseConstSection(doc string) []string {
+	tkn := html.NewTokenizer(strings.NewReader(doc))
+	var vals []string
+	var isPkgConstSection bool
+	for {
+
+		tt := tkn.Next()
+		switch {
+		case tt == html.ErrorToken:
+			return vals
+
+		case tt == html.StartTagToken:
+			if isPkgConstSection {
+				continue
+			}
+			t := tkn.Token()
+			isPkgConstSection = t.String() == sectionToParse
+
+		case tt == html.EndTagToken:
+			t := tkn.Token()
+			if t.String() == "</section>" && isPkgConstSection {
+				return vals
+			}
+		case tt == html.TextToken:
+			t := tkn.Token()
+			if isPkgConstSection {
+				vals = append(vals, t.Data)
+			}
+		}
+	}
+	return vals
+}
+
+func parsePkgConstFromSection(doc string) []string {
+	constList := []string{}
+	sectionItems := parseConstSection(doc)
+	for _, i := range sectionItems {
+		con := strings.TrimSpace(i)
+		// Skip if comment
+		if strings.HasPrefix(con, "//") {
+			continue
+		}
+		if len(con) == 0 {
+			continue
+		}
+		conKeySplit := strings.Split(con, "=")
+		// Skip if is not in key=value format
+		if len(conKeySplit) != 2 {
+			continue
+		}
+		// Print const name
+		constList = append(constList, conKeySplit[0])
+	}
+	return constList
+}
+
+func main() {
+	content, err := getPackageDocContent()
+	if err != nil {
+		log.Fatal(err)
+	}
+	consts := parsePkgConstFromSection(string(content))
+	fmt.Println(consts)
+}
